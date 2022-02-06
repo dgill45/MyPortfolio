71e4a2184ac92e87fbd09dc01317462f830081fe
donellgill <donell.gill@outlook.com>
2022-02-06 18:30:18 -0500

    setup project page basic structure

--- src/components/About.js
+++ src/components/About.js
@@ -27,8 +27,8 @@ export default function About(){
                 </div>
                 <div className='lg:max-w-lg lg:w-full md:w-1/2 w-5/6'>
                     <img className="object-cover object-center rounded"
-                        alt="hero"
-                        src="./coding.svg" />
+                      src= "https://avatars.githubusercontent.com/u/70354925?s=400&u=52b06c14e1b4ac9494e273e2a11520b20078c201&v=4"
+                      alt = "me" />
                 </div>
             </div>
         </section>
